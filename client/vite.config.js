25a699fcf43ed742ee0792b0f0f618d4c49d6fe5
Shino <184890980+kakinotane12@users.noreply.github.com>
2025-11-27 01:42:05 +0900

    feat: Add Vite configuration for development server with API proxy and HMR.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: '../',
   server: {
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
